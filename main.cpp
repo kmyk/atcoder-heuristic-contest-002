28fe189c0270d0459a703ea0ff5e9f270141c4be
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-04-25 20:09:17 +0900

    とりあえずビームサーチする

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ private:
 
 constexpr int N = 50;
 
+constexpr array<int, 4> DIRS = {{0, 1, 2, 3}};
 constexpr array<char, 4> DIR_LETTER = {'U', 'D', 'R', 'L'};
 constexpr array<int, 4> DIR_Y = {-1, 1, 0, 0};
 constexpr array<int, 4> DIR_X = {0, 0, 1, -1};
@@ -38,8 +39,18 @@ inline bool is_on_tiles(int y, int x) {
     return 0 <= y and y < N and 0 <= x and x < N;
 }
 
+struct beam_state {
+    string command;
+    vector<bool> used;
+    int score;
+    int8_t y;
+    int8_t x;
+};
+
 template <class RandomEngine>
-string solve(const int sy, const int sx, const array<array<int, N>, N>& tile, const array<array<int, N>, N>& point, RandomEngine& gen) {
+string solve(const int sy, const int sx, const array<array<int, N>, N>& tile, const array<array<int, N>, N>& point, RandomEngine& gen, chrono::high_resolution_clock::time_point clock_end) {
+    chrono::high_resolution_clock::time_point clock_begin = chrono::high_resolution_clock::now();
+
     int M = 0;
     REP (y, N) {
         REP (x, N) {
@@ -47,41 +58,63 @@ string solve(const int sy, const int sx, const array<array<int, N>, N>& tile, co
         }
     }
 
-    // random walk
-    int y = sy;
-    int x = sx;
-    vector<bool> used(M);
-    used[tile[y][x]] = true;
-    int score = point[y][x];
-    string ans;
-    while (true) {
-        array<int, 4> dirs = {{0, 1, 2, 3}};
-        shuffle(ALL(dirs), gen);
-        bool found = false;
-        for (int dir : dirs) {
-            int ny = y + DIR_Y[dir];
-            int nx = x + DIR_X[dir];
-            if (is_on_tiles(ny, nx) and not used[tile[ny][nx]]) {
-                found = true;
-                ans.push_back(DIR_LETTER[dir]);
-                y = ny;
-                x = nx;
-                used[tile[y][x]] = true;
-                score += point[y][x];
-                break;
+    vector<shared_ptr<beam_state> > cur;
+    {
+        shared_ptr<beam_state> initial = make_shared<beam_state>();
+        initial->y = sy;
+        initial->x = sx;
+        initial->used.resize(M);
+        initial->used[tile[sy][sx]] = true;
+        initial->score = point[sy][sx];
+        cur.push_back(initial);
+    }
+    shared_ptr<beam_state> result = cur.back();
+
+    while (not cur.empty()) {
+        chrono::high_resolution_clock::time_point clock_now = chrono::high_resolution_clock::now();
+        if (clock_end <= clock_now) {
+            break;
+        }
+
+        vector<shared_ptr<beam_state> > prv;
+        cur.swap(prv);
+        for (auto& s : prv) {
+            for (int dir : DIRS) {
+                int ny = s->y + DIR_Y[dir];
+                int nx = s->x + DIR_X[dir];
+                if (is_on_tiles(ny, nx) and not s->used[tile[ny][nx]]) {
+                    shared_ptr<beam_state> t = make_shared<beam_state>(*s);
+                    t->y = ny;
+                    t->x = nx;
+                    t->command.push_back(DIR_LETTER[dir]);
+                    t->used[tile[ny][nx]] = true;
+                    t->score += point[ny][nx];
+                    cur.push_back(t);
+                }
             }
         }
-        if (not found) {
-            break;
+        constexpr int WIDTH = 1000;
+        if (cur.size() > WIDTH) {
+            partial_sort(cur.begin(), cur.begin() + WIDTH, cur.end(), [&](const shared_ptr<beam_state>& a, const shared_ptr<beam_state>& b) {
+                return a->score > b->score;
+            });
+            cur.resize(WIDTH);
+        }
+        if (not cur.empty() and result->score < cur.front()->score) {
+            result = cur.front();
         }
     }
 
-    cerr << "ans = " << ans << endl;
-    cerr << "score = " << score << endl;
-    return ans;
+    cerr << "ans = " << result->command << endl;
+    cerr << "score = " << result->score << endl;
+    return result->command;
 }
 
 int main() {
+    constexpr auto TIME_LIMIT = chrono::milliseconds(2000);
+    chrono::high_resolution_clock::time_point clock_begin = chrono::high_resolution_clock::now();
+    xor_shift_128 gen(20210425);
+
     int sy, sx; cin >> sy >> sx;
     array<array<int, N>, N> tile;
     REP (y, N) {
@@ -95,8 +128,7 @@ int main() {
             cin >> point[y][x];
         }
     }
-    xor_shift_128 gen;
-    string ans = solve(sy, sx, tile, point, gen);
+    string ans = solve(sy, sx, tile, point, gen, clock_begin + chrono::duration_cast<chrono::milliseconds>(TIME_LIMIT * 0.95));
     cout << ans << endl;
     return 0;
 }
